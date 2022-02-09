3be3077bda82e630b8d837226e13dad41861da51
kyle lee <kyleleeca@gmail.com>
2022-02-09 14:58:13 -0500

    Add route handlers for GET requests

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const app = express();
 
-// app.get()
-// app.post()
-// app.put()
-// app.delete()
+
+// Hardcode items for now 
+// To be replaced with the real items from DB
+const items = [
+    {id:1, name: 'item1'},
+    {id:2, name: 'item2'},
+    {id:3, name: 'item3'},
+];
 
 app.get('/', (req,res) => {
     res.send('This is the root of the website.');
 });
 app.get('/api/items', (req, res) => {
     res.send([1, 2, 3]);
-})
+});
 
+// api/items/itemID
+app.get('/api/items/:id', (req,res) => {
+    const item = items.find(item => item.id === parseInt(req.params.id));
+    
+    if (!item) {
+        res.status(404).send('The item with the given ID was not found');
+    }
+    res.send(item);
+
+});
 
 const port = process.env.PORT || 3000;
 app.listen(3000, ()=> {
