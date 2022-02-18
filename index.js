fdabcdcd08c7a0feb57d1d20cca7992e82a7208d
kyle lee <kyleleeca@gmail.com>
2022-02-17 23:40:48 -0500

    Add routes to handle customers

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const Joi = require('joi');
 const items = require('./routes/items');
 const app = express();
 const homepage = require('./routes/homepage');
+const customers = require('./routes/customers');
+
 app.use(express.json()); // Piece of middleware to parse request body 
 app.use('/api/items', items); // any routes that start with first argument, use this router 'items' 
 app.use('/', homepage);
-
+app.use('/api/customers', customers);
+mongoose.connect('mongodb://localhost/express')
+    .then(() =>  console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to DB...'));
+    
 const port = process.env.PORT || 3000;
 app.listen(3000, ()=> {
     console.log(`Listening on port ${port}`)
